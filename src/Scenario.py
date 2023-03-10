1a8a1411d8c2ad3d0623fcf08144c8cc607fb96d
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-03-10 09:48:26 +0100

    Adding class Scenario

---
+++ src/Scenario.py
@@ -0,0 +1,15 @@
+from .Constants import *
+from .Utils import *
+
+class Scenario:
+    r"""Static function defining and declaring several scenarios for the $t \longmapsto \theta(t)$ values
+    """
+    
+    @staticmethod
+    def scenario_1(t: float):
+        """Function defining the scenario 1
+
+        Args:
+            t (float): Time
+        """        
+        pass
