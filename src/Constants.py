4f40e8d1f17549f2abfba350802d36e87e50af8b
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-03-18 13:45:19 +0100

    Adding Z-scores declaration for MC CI in Constants()

--- src/Constants.py
+++ src/Constants.py
@@ -17,3 +17,15 @@ class Constants:
     # Defining the available numerical schemes
     Contract = Enum("Contract", ["CALL", "PUT"])
 
+    # Z-score for Monte-Carlo confidence intervals
+    Z_SCORES = {
+        80: 1.282,
+        85: 1.440,
+        90: 1.645,
+        95: 1.960,
+        99: 2.576,
+        99.5: 2.807,
+        99.9: 3.291
+    }
+    # Reference: https://www.alchemer.com/resources/blog/how-to-calculate-confidence-intervals/
+
