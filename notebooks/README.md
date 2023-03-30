3923484347899ffd6cf6a5349e9f3b60531ca5a3
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-03-30 22:14:39 +0200

    Adding intermediary README for notebook/ subfolder + adding the five scenario static methods for further implementation

---
+++ notebooks/README.md
@@ -0,0 +1,14 @@
+# Notebooks
+
+
+## Outline
+
+Four notebooks have been developed and maintained to enhance the user experience and allow us to split our work by theme
+
+1. Interest-rate models
+2. Equity models
+3. Pricing
+4. Analysis
+
+
+*Feel free to open a new PR with your own notebook if you want to aggregate our current work*
--- src/Scenario.py
+++ src/Scenario.py
@@ -6,10 +6,62 @@ class Scenario:
     """
     
     @staticmethod
-    def scenario_1(t: float):
+    def scenario_1(t: float) -> Any:
         """Function defining the scenario 1
 
         Args:
             t (float): Time
+
+        Returns:
+            Any: $t \longmapsto \theta(t)$ array values
         """        
-        pass
+        return NotImplemented
+
+    @staticmethod
+    def scenario_2(t: float) -> Any:
+        """Function defining the scenario 2
+
+        Args:
+            t (float): Time
+
+        Returns:
+            Any: $t \longmapsto \theta(t)$ array values
+        """
+        return NotImplemented
+    
+
+    @staticmethod
+    def scenario_3(t: float) -> Any:
+        """Function defining the scenario 3
+
+        Args:
+            t (float): Time
+
+        Returns:
+            Any: $t \longmapsto \theta(t)$ array values
+        """
+        return NotImplemented
+    
+    @staticmethod
+    def scenario_4(t: float) -> Any:
+        """Function defining the scenario 4
+
+        Args:
+            t (float): Time
+
+        Returns:
+            Any: $t \longmapsto \theta(t)$ array values
+        """
+        return NotImplemented
+    
+    @staticmethod
+    def scenario_5(t: float) -> Any:
+        """Function defining the scenario 5
+
+        Args:
+            t (float): Time
+
+        Returns:
+            Any: $t \longmapsto \theta(t)$ array values
+        """
+        return NotImplemented
